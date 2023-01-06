8325375e9f304395addd2c1a17563a312cc9ccb1
CaptjayZorin <captjay98@gmail.com>
2023-01-06 04:08:34 +0100

    done

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,2 +1,10 @@
 #usr/bin/env python3
 """duck-typed annotations"""
+
+
+def safe_first_element(lst):
+    """returns first safe element"""
+    if lst:
+        return lst[0]
+    else:
+        return None
