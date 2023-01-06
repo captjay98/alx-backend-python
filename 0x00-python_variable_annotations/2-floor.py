3b1e6df05f3fb8a4bc0fb289c6c6c49cd8e5481d
CaptjayZorin <captjay98@gmail.com>
2023-01-06 03:20:28 +0100

    Task 2 fix

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+""" Type annotated function that rounds a number down """
 import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """Rounds a number down"""
     return math.floor(n)
