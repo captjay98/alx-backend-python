150f417549915a246148720180ec4e3c516fc097
CaptjayZorin <captjay98@gmail.com>
2023-01-06 03:22:47 +0100

    Task 1 fixed

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """A function that returns concatenated strings"""
 
+
 def concat(str1: str, str2: str) -> str:
     """Concatenated two strings"""
     return (str1 + str2)
